585e913524d3e519f995506fa0d80e05666e6310
Edoardo Alberti <edoalberti99@gmail.com>
2025-01-18 00:16:24 +0100

    fix

--- app/components/input/Button.vue
+++ app/components/input/Button.vue
@@ -1,6 +1,6 @@
 <template>
    <button
-      class="__clickabl relative h-fit overflow-hidden rounded-3xl font-title font-medium text-white"
+      class="__clickable relative h-fit overflow-hidden rounded-3xl font-title font-medium text-white"
       :class="{
          'bg-candy-apple-red px-5 py-2 md:px-8': type === 'primary',
          'rounded-xl bg-charleston-green px-4 py-2': type === 'icon',
